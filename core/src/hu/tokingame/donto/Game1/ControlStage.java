7efb9b7b9db6cd1f79fb1ac448198655ad4c51e8
davidmatyas <davimatyi@outlook.com>
2017-02-03 10:08:06 +0100

    a whole new world

--- core/src/hu/tokingame/donto/Game1/ControlStage.java
+++ core/src/hu/tokingame/donto/Game1/ControlStage.java
@@ -2,11 +2,15 @@ package hu.tokingame.donto.Game1;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 import hu.tokingame.donto.Global.Globals;
+import hu.tokingame.donto.MenuScreen.MenuScreen;
 import hu.tokingame.donto.MyBaseClasses.MyLabel;
 import hu.tokingame.donto.MyBaseClasses.MyStage;
+import hu.tokingame.donto.MyBaseClasses.MyTextButton;
 import hu.tokingame.donto.MyGdxGame;
 
 /**
@@ -20,7 +24,7 @@ public class ControlStage extends MyStage {
     public ControlStage(MyGdxGame game, GameStage sg) {
         super(new ExtendViewport(Globals.WORLD_WIDTH,Globals.WORLD_HEIGHT,new OrthographicCamera(Globals.WORLD_WIDTH,Globals.WORLD_HEIGHT)),new SpriteBatch(), game);
         gameStage = sg;
-        addActor(new MyLabel("Sutyk",MyLabel.style1));
+
     }
 
 
@@ -36,6 +40,19 @@ public class ControlStage extends MyStage {
 
     @Override
     public void init() {
-
+        addActor(new MyTextButton("Kilépés"){
+            @Override
+            protected void init() {
+                super.init();
+                setPosition(Globals.WORLD_WIDTH-this.getWidth(), Globals.WORLD_HEIGHT-this.getHeight());
+                addListener(new ClickListener(){
+                    @Override
+                    public void clicked(InputEvent event, float x, float y) {
+                        super.clicked(event, x, y);
+                        game.setScreen(new MenuScreen(game));
+                    }
+                });
+            }
+        });
     }
 }
--- core/src/hu/tokingame/donto/Game1/GameStage.java
+++ core/src/hu/tokingame/donto/Game1/GameStage.java
@@ -5,10 +5,14 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 import hu.tokingame.donto.MenuScreen.MenuScreen;
 import hu.tokingame.donto.MyBaseClasses.MyStage;
+import hu.tokingame.donto.MyBaseClasses.WorldBodyEditorLoader;
 import hu.tokingame.donto.MyGdxGame;
 
 /**
@@ -20,6 +24,12 @@ public class GameStage extends MyStage {
 
     private ControlStage controlStage;
 
+    World world;
+    Box2DDebugRenderer box2DDebugRenderer;
+    WorldBodyEditorLoader loader;
+
+    float elapsedTime = 0;
+
     int rdm(int a, int b){return (int)(Math.random()*(b-a+1)+a);}
 
 
@@ -27,7 +37,14 @@ public class GameStage extends MyStage {
         super(new ExtendViewport(16,9,new OrthographicCamera(16,9)), new SpriteBatch(), game);
         controlStage = new ControlStage(game, this);
         inputMultiplexer.addProcessor(this);
+        inputMultiplexer.addProcessor(controlStage);
         Gdx.input.setInputProcessor(inputMultiplexer);
+    }
+    @Override
+    public void init() {
+        world = new World(new Vector2(0, -20), false);
+        box2DDebugRenderer = new Box2DDebugRenderer();
+        loader = new WorldBodyEditorLoader(Gdx.files.internal("phys.json"));
     }
 
     @Override
@@ -38,15 +55,14 @@ public class GameStage extends MyStage {
         return false;
     }
 
-    @Override
-    public void init() {
-    }
 
 
     @Override
     public void act(float delta) {
         super.act(delta);
         controlStage.act();
+        world.step(delta, 10, 10);
+        elapsedTime  += delta;
     }
 
     @Override
