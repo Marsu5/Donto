c0fbd21ac80f9e8846e8547e2bcd752fe7b9b4c8
Loner99 <kovacs.zolika99@gmail.com>
2017-02-03 11:19:22 +0100

    DeathStage update

--- core/src/hu/tokingame/donto/Game1/DeathStage.java
+++ core/src/hu/tokingame/donto/Game1/DeathStage.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 import hu.tokingame.donto.LoadingScreen.LoadingStage;
+import hu.tokingame.donto.MyBaseClasses.MyLabel;
 import hu.tokingame.donto.MyBaseClasses.MyStage;
 import hu.tokingame.donto.MyGdxGame;
 
@@ -18,12 +19,21 @@ public class DeathStage extends MyStage {
     public DeathStage(Viewport viewport, Batch batch, MyGdxGame game, int score) {
         super(viewport, batch, game);
 
+        addActor(new MyLabel("You Died. \n Your score was:"+score, MyLabel.style1){
+            @Override
+            public void init() {
+                super.init();
+                this.setPosition(200, 600);
+            }
+        });
     }
 
 
     @Override
     public void init() {
         DeathStage = this;
+
+
 
     }
 
