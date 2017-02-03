5bdbbafd0c60fae7136126a4e95a8932e93544b3
Loner99 <kovacs.zolika99@gmail.com>
2017-02-03 11:49:42 +0100

    Már csak háttér kell

--- core/src/hu/tokingame/donto/Game1/DeathStage.java
+++ core/src/hu/tokingame/donto/Game1/DeathStage.java
@@ -1,12 +1,19 @@
 package hu.tokingame.donto.Game1;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+import hu.tokingame.donto.Global.Assets;
+import hu.tokingame.donto.Global.Globals;
 import hu.tokingame.donto.LoadingScreen.LoadingStage;
+import hu.tokingame.donto.MenuScreen.MenuScreen;
 import hu.tokingame.donto.MyBaseClasses.MyLabel;
 import hu.tokingame.donto.MyBaseClasses.MyStage;
+import hu.tokingame.donto.MyBaseClasses.MyTextButton;
 import hu.tokingame.donto.MyGdxGame;
+import jdk.nashorn.internal.objects.Global;
 
 /**
  * Created by Zoli on 2017.02.03..
@@ -23,9 +30,10 @@ public class DeathStage extends MyStage {
             @Override
             public void init() {
                 super.init();
-                this.setPosition(200, 600);
+                this.setPosition(Globals.WORLD_WIDTH/2-this.getWidth()/2f, Globals.WORLD_HEIGHT/2-this.getHeight()/2f);
             }
         });
+
     }
 
 
@@ -33,6 +41,35 @@ public class DeathStage extends MyStage {
     public void init() {
         DeathStage = this;
 
+        addActor(new MyTextButton("Play again"){
+            @Override
+            protected void init() {
+                super.init();
+                setPosition(10, 10);
+                addListener(new ClickListener(){
+                    @Override
+                    public void clicked(InputEvent event, float x, float y) {
+                        super.clicked(event, x, y);
+                        game.setScreen(new GameScreen(game));
+                    }
+                });
+            }
+        });
+
+        addActor(new MyTextButton("Exit"){
+            @Override
+            protected void init() {
+                super.init();
+                setPosition(Globals.WORLD_WIDTH-100, 10);
+                addListener(new ClickListener(){
+                    @Override
+                    public void clicked(InputEvent event, float x, float y) {
+                        super.clicked(event, x, y);
+                        game.setScreen(new MenuScreen(game));
+                    }
+                });
+            }
+        });
 
 
     }
