a26cfc4922a718549da80cf7ef7b16cc0295121c
davidmatyas <davimatyi@outlook.com>
2017-02-03 12:34:09 +0100

    malacvalag elröpül

--- core/src/hu/tokingame/donto/Bodies/DeadPigHead.java
+++ core/src/hu/tokingame/donto/Bodies/DeadPigHead.java
@@ -1,5 +1,6 @@
 package hu.tokingame.donto.Bodies;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
 
@@ -23,5 +24,11 @@ public class DeadPigHead extends WorldActorGroup {
         addActor(actor);
         setPosition(x,y);
         addToWorld();
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        getBody().applyForceToCenter(new Vector2(-150*delta, 0), true);
     }
 }
